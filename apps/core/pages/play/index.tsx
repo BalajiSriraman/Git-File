d69c9d9e4af84e647c114af0db4bf4fb4c699901
BalajiSriraman <balajisr000@gmail.com>
2023-12-30 14:58:59 +0530

    Refactor ButtonDemo component

--- apps/core/pages/play/index.tsx
+++ apps/core/pages/play/index.tsx
@@ -1,15 +1,15 @@
-// import { Button } from "@components/shadeCnUI/Button";
+import { Button } from '@components/shadeCnUI/Button';
 
-// export default function ButtonDemo() {
-//   return (
-//     <div className="w-100 h-100 flex justify-center items-center">
-//       <Button
-//         className="bg-white border-1 hover:bg-slate-300 text-black"
-//         variant={"outline"}
-//         size={"sm"}
-//       >
-//         Button
-//       </Button>
-//     </div>
-//   );
-// }
+export default function ButtonDemo() {
+  return (
+    <div className="w-100 h-100 flex justify-center items-center">
+      <Button
+        className="bg-white border-1 hover:bg-slate-300 text-black"
+        variant={'outline'}
+        size={'sm'}
+      >
+        Button
+      </Button>
+    </div>
+  );
+}
